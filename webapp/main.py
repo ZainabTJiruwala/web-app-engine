36ba7355f3159c9d65a847df462ccf7610cf69c6
ZainabTJiruwala <zainab.tmj@gmail.com>
2026-04-08 20:57:05 +0530

    Update main.py

--- webapp/main.py
+++ webapp/main.py
@@ -1,9 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 import os
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-  return render_template('index.html',title="Home Page")
-if __name__ == '__main__':
-  app.run(host='127.0.0.1',port=8080,debug=True)
+    # This looks for index.html inside the 'templates' folder
+    return render_template('index.html', title="Home Page")
 
+if __name__ == '__main__':
+    # Cloud Shell uses port 8080 by default
+    app.run(host='127.0.0.1', port=8080, debug=True)
