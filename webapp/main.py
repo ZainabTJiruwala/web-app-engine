fc9afd89a2e80bc781cb08cff89990fa9794da24
ZainabTJiruwala <zainab.tmj@gmail.com>
2026-04-08 20:35:45 +0530

    Fix main module check syntax in main.py

--- webapp/main.py
+++ webapp/main.py
@@ -4,6 +4,6 @@ app=Flask(__name__)
 @app.route('/')
 def home():
   return render_template('index.html',title="Home Page")
-if __main__=='__name__':
+if __name__=='__main__':
   app.run(host='127.0.0.1',port=8080,debug=True)
 
